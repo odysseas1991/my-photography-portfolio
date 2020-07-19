2170c74d480f745d99cba9f8161171ceefde5e56
odysseas1991 <19oddy91@gmail.com>
2020-07-19 08:17:39 -0900

    Added some styles in Projects page. Also, added the background image with description on hover.

--- src/components/projectDetails/projectDetails.component.jsx
+++ src/components/projectDetails/projectDetails.component.jsx
@@ -2,27 +2,23 @@ import React from 'react';
 
 import './projectDetails.styles.scss';
 
-const ProjectDetails = ({ title, images }) => {
+const ProjectDetails = ({ images }) => {
   console.log('Images: ', images);
   return (
-    <div className='content'>
-      <h3 className='title'>{title}</h3>
-      <div className='images-details'>
-        {images
-          ? images.map((imageDetails) => (
-              <div className='image' key={imageDetails.id}>
-                <div
-                  className='background-image'
-                  style={{ backgroundImage: `url(${imageDetails.imageUrl})` }}
-                />
-                <div className='image-content'>
-                  <h1 className='title'>{imageDetails.title}</h1>
-                  <p className='description'>{imageDetails.description}</p>
-                </div>
+    <div className='images-details'>
+      {images
+        ? images.map((imageDetails) => (
+            <div className='image' key={imageDetails.id}>
+              <div
+                className='background-image'
+                style={{ backgroundImage: `url(${imageDetails.imageUrl})` }}
+              />
+              <div className='image-content'>
+                <p className='description'>{imageDetails.description}</p>
               </div>
-            ))
-          : []}
-      </div>
+            </div>
+          ))
+        : []}
     </div>
   );
 };
--- src/components/projectPreview/projectPreview.component.jsx
+++ src/components/projectPreview/projectPreview.component.jsx
@@ -5,10 +5,8 @@ import './projectPreview.styles.scss';
 import ProjectDetails from '../projectDetails/projectDetails.component';
 
 const ProjectPreview = ({ title, images }) => {
-  console.log(title);
-  console.log(images);
   return (
-    <div className='content'>
+    <div className='project-page'>
       <div className='title'>{title}</div>
       <div className='projects-content'>
         <ProjectDetails images={images} />
